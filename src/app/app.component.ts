cbbea9db2cc964095929c2bf292a27e4972167a8
THANUKRISHNA-K <thanukrishna.1709053@srec.ac.in>
2021-06-25 19:15:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase, AngularFireList} from '@angular/fire/database'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  p:number=1;
+  lst={
+    names:"",
+    id:"",
+    phone:"",
+    email:"",
+    dept:"",
+    college:"",
+    locality:""
+  }
+  data:any;
   title = 'firebaseinteg';
+  constructor(private auth:AngularFireDatabase){
+    const items:AngularFireList<any>=auth.list('userlist');
+    items.valueChanges().subscribe(data=>this.data=data);
+  }
+  add(){
+    this.auth.list("userlist").push(this.lst);
+  }
+  remov(i:any){
+  let rem=this.auth.list("userlist");
+  rem.remove(i);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule  } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { NgxPaginationModule} from 'ngx-pagination';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCiCM_psh9M79s5_pV8WDdLoh4xlvH0goA",
+  authDomain: "sample-d8234.firebaseapp.com",
+  databaseURL: "https://sample-d8234-default-rtdb.firebaseio.com",
+  projectId: "sample-d8234",
+  storageBucket: "sample-d8234.appspot.com",
+  messagingSenderId: "751663947257",
+  appId: "1:751663947257:web:2de882b4d33c7c64907128"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +26,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
