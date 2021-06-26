b8f2a894277dfecc8dbd4c6192b8d1129f49c46b
THANUKRISHNA-K <thanukrishna.1709053@srec.ac.in>
2021-06-26 17:02:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, AngularFireList} from '@angular/fire/database'
+import { AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+import {AngularFireStorage} from '@angular/fire/storage';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
+  filePath:String | undefined;
+  filedata=""
   p:number=1;
   lst={
     names:"",
@@ -15,19 +17,45 @@ export class AppComponent {
     email:"",
     dept:"",
     college:"",
-    locality:""
+    locality:"",
   }
   data:any;
   title = 'firebaseinteg';
-  constructor(private auth:AngularFireDatabase){
+  constructor(private auth:AngularFireDatabase,private afStorage: AngularFireStorage){
     const items:AngularFireList<any>=auth.list('userlist');
-    items.valueChanges().subscribe(data=>this.data=data);
+    items.snapshotChanges().subscribe(data=>{
+      let a:any=[];
+      data.forEach(v=>{let b:any={key:v.key, data:v.payload.val()};
+      a.push({key:b.key, ...b.data})
+    });
+   
+    this.data=a;
+    });
   }
-  add(){
+  upload(event:any) {    
+    this.filePath = event.target.files[0]
+  }
+  uploadImage(){
+    if(this.filedata==""){
+      alert("please upload file")
+    }
+    else{
+      console.log(this.filePath)
+      alert("image uploaded successfully")
+      this.afStorage.upload('/images'+Math.random( )+this.filePath,  this.filePath, );
+    }
+      
+  }
+
+  add()
+  {
     this.auth.list("userlist").push(this.lst);
   }
-  remov(i:any){
-  let rem=this.auth.list("userlist");
-  rem.remove(i);
+  remov(i:any)
+  {
+    console.log({ i });
+    this.auth.object(`userlist/${i}`).remove();
   }
+
+
 }
